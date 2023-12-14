cbe098db2fcac69313382e0c9503731f19974af5
Sanlinn777 <150360445+Sanlinn777@users.noreply.github.com>
2023-12-15 00:06:09 +0630

    Update SI_new.py

--- SI_new.py
+++ SI_new.py
@@ -112,7 +112,7 @@ else:
     #Tsps Data Frame
     df_tsp= df_si.groupby(['Tsp'], as_index= False).agg({'YEAR':'count'})  # Updated column name
     df_tsp = df_tsp.sort_values('YEAR', ascending= False)
-    df_tsp = df_tsp.rename(columns={'YEAR':'Total Referral"},inplace=True)
+    df_tsp = df_tsp.rename(columns={'YEAR':'Total Referral'},inplace=True)
 
     st.subheader('Township with Highest Referral')
     st.write(df_tsp, use_container_width=True)
